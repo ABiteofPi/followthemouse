38cf2803c418e73579f82c2361813b44a3877d0e
ABiteofPi <payabiteofpi@gmail.com>
2026-01-28 19:23:40 +0100

    fix: changed repo name

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  base: '/followthemouse/',
+  plugins: [vue(), vueDevTools()],
+  base: '/password-strength-meter/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
