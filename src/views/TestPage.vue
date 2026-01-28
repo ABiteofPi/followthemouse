f40d356378593ef74f6f4325a974719fedd9db10
ABiteofPi <payabiteofpi@gmail.com>
2026-01-28 01:22:50 +0100

    fix: updated styles

--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -5,29 +5,40 @@ import { analyzePassword } from '../types/logic';
 const password = ref('');
 const result = computed(() => analyzePassword(password.value));
 
-function getBarColor(level: string) {
-  const colors: Record<string, string> = {
-    'Too Weak': '#ff4d4d',
-    'Weak': '#ffa500',
-    'Medium': '#ffff00',
-    'Strong': '#90ee90',
-    'Very Strong': '#008000'
-  };
-  return colors[level];
-}
+// function getBarColor(level: string) {
+//   const colors: Record<string, string> = {
+//     'Too Weak': '#ff4d4d',
+//     'Weak': '#ffa500',
+//     'Medium': '#ffff00',
+//     'Strong': '#90ee90',
+//     'Very Strong': '#008000'
+//   };
+//   return colors[level];
+// }
+
+const strengthColor = computed(() => {
+  if (result.value.score === 0) return ''
+  if (result.value.level === 'Too Weak') return 'too-weak'
+  if (result.value.level === 'Weak') return 'weak'
+  if (result.value.level === 'Moderate') return 'moderate'
+  if (result.value.level === 'Strong') return 'strong'
+  if (result.value.level === 'Very Strong') return 'very-strong'
+});
+
 </script>
 
 <template>
   <div class="d-flex flex-column align-items-center h-100 py-5">
     <h1 class="my-5 doto title px-5">Password Strength Meter</h1>
     <div class="d-flex flex-column gap-2 mt-5">
-      <input v-model="password" type="password" class="input" placeholder="Type a password..." />
+      <input v-model="password" type="password" class="input" :class="strengthColor" placeholder="Type a password..." />
 
-      <div class="meter d-flex align-items-center justify-content-center px-4">
+      <!-- <div class="meter d-flex align-items-center justify-content-center px-4">
         <div class="progress" :style="{ width: result.score + '%', backgroundColor: getBarColor(result.level) }"></div>
-      </div>
+      </div> -->
 
-      <p class="doto px-4 py-2 mb-2 align-self-center">Strength: <strong>{{ result.level }}</strong></p>
+      <p class="doto px-4 py-2 mb-2 align-self-center">Strength: <strong :class="strengthColor">{{ result.level
+      }}</strong></p>
 
       <div v-if="result.feedback.length > 0" class="px-4 py-3 d-flex flex-column gap-2 lh-1 tip-box">
         <div class="d-flex flex-row gap-2">
@@ -57,7 +68,7 @@ function getBarColor(level: string) {
   border-radius: 50px;
   min-width: 500px;
   background-color: #090014;
-  color: #fff;
+  color: #fff !important;
   box-shadow: 0 4px 25px rgba(0, 0, 0, 0.3);
   transition: background 0.4s ease;
   transition: border 0.3s ease;
@@ -73,6 +84,46 @@ function getBarColor(level: string) {
     border: 1px solid #cccccc8a;
     box-shadow: 0 2px 25px rgba(255, 255, 255, 0.1);
     background-color: #0e011f;
+  }
+}
+
+.too-weak {
+  color: #ff4d4d;
+
+  &:focus-visible {
+    border: 1px solid #ff4d4d;
+  }
+}
+
+.weak {
+  color: #ffa500;
+
+  &:focus-visible {
+    border: 1px solid #ffa500;
+  }
+}
+
+.moderate {
+  color: #ffff00;
+
+  &:focus-visible {
+    border: 1px solid #ffff00;
+  }
+}
+
+.strong {
+  color: #90ee90;
+
+  &:focus-visible {
+    border: 1px solid #90ee90;
+  }
+}
+
+.very-strong {
+  color: #00c700;
+
+  &:focus-visible {
+    border: 1px solid #00c700;
   }
 }
 
